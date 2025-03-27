0a4f6992f38348810698ab63d14716252e119258
JongasD <lopesjonasdaniel@gmail.com>
2025-03-27 15:39:06 -0300

    Update aova.py

--- aova.py
+++ aova.py
@@ -27,3 +27,25 @@ print("welcome to aova")
 print("welcome to aova")
 
 print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
+
+print("welcome to aova")
